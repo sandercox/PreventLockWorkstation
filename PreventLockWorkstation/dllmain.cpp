67a222e127422631196b1becdef7286ac78a29ed
Sander Cox <sander@paralleldimension.nl>
2015-11-12 11:11:36 +0100

    Added optional logging and forcing LoadAppInit_DLLs

--- PreventLockWorkstation/dllmain.cpp
+++ PreventLockWorkstation/dllmain.cpp
@@ -4,11 +4,14 @@
 // LockWorkStation official type definition
 typedef BOOL(WINAPI* _LockWorkStation)();
 
+//#define LOGTRACE 1
+
 BOOL WINAPI BlockedLockWorkStation()
 {
-	std::ofstream log("d:\\work\\dll_log.txt", std::ios::app | std::ios::out);
+#ifdef LOGTRACE
+	std::ofstream log("d:\\work\\dll_log64.txt", std::ios::app | std::ios::out);
 	log << "Blocked LockWorkStation" << std::endl;
-
+#endif
 	return TRUE;
 }
 
@@ -17,19 +20,53 @@ BOOL APIENTRY DllMain( HMODULE hModule,
                        LPVOID lpReserved
 					 )
 {
+#ifdef LOGTRACE
+	std::ofstream log("d:\\work\\dll_log64.txt", std::ios::app | std::ios::out);
+	log << "DLL Main... ";
+
+	wchar_t fname[255];
+	GetModuleFileName(hModule, fname, 255);
+	std::wstring sWide(fname);
+	std::string sFile(sWide.begin(), sWide.end());
+	log << " " << sFile << " -- ";
+	
+
+#endif
 	_LockWorkStation TrueLockWorkStation = (_LockWorkStation)GetProcAddress(GetModuleHandle(L"user32"), "LockWorkStation");
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-		Mhook_SetHook((PVOID*)&TrueLockWorkStation, BlockedLockWorkStation);
+	{
+#ifdef LOGTRACE
+		log << "DLL_PROCESS_ATTACH";
+#endif
+		if (Mhook_SetHook((PVOID*)&TrueLockWorkStation, BlockedLockWorkStation))
+		{
+#ifdef LOGTRACE
+			log << "  -- hook set!";
+#endif
+		}
+	}
 		break;
 	case DLL_THREAD_ATTACH:
+#ifdef LOGTRACE
+		log << "DLL_THREAD_ATTACH";
+#endif
 		break;
 	case DLL_THREAD_DETACH:
+#ifdef LOGTRACE
+		log << "DLL_THREAD_DETACH";
+#endif
 		break;
 	case DLL_PROCESS_DETACH:
+#ifdef LOGTRACE
+		log << "DLL_PROCESS_DETACH";
+#endif
 		Mhook_Unhook((PVOID*)&TrueLockWorkStation);
 		break;
 	}
+#ifdef LOGTRACE
+	log << std::endl;
+#endif
 	return TRUE;
 }
