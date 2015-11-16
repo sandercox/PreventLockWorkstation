61916dc33aad20cd7d803baff5ef93120012f728
Sander Cox <sander@paralleldimension.nl>
2015-11-16 09:38:24 +0100

    Update to include debug credentials to also be able to open processes owned by the system. Fixed hp remote graphics process name.

--- InjectDLL/InjectDLL.cpp
+++ InjectDLL/InjectDLL.cpp
@@ -3,6 +3,77 @@
 
 #include "stdafx.h"
 
+#pragma comment(lib, "advapi32.lib")
+
+#ifdef _DEBUG
+#define LOG_DEBUG
+#endif
+
+BOOL SetPrivilege(
+	HANDLE hToken,          // access token handle
+	LPCTSTR lpszPrivilege,  // name of privilege to enable/disable
+	BOOL bEnablePrivilege   // to enable or disable privilege
+	)
+{
+	TOKEN_PRIVILEGES tp;
+	LUID luid;
+
+	if (!LookupPrivilegeValue(
+		NULL,            // lookup privilege on local system
+		lpszPrivilege,   // privilege to lookup 
+		&luid))        // receives LUID of privilege
+	{
+		printf("LookupPrivilegeValue error: %u\n", GetLastError());
+		return FALSE;
+	}
+
+	tp.PrivilegeCount = 1;
+	tp.Privileges[0].Luid = luid;
+	if (bEnablePrivilege)
+		tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+	else
+		tp.Privileges[0].Attributes = 0;
+
+	// Enable the privilege or disable all privileges.
+
+	if (!AdjustTokenPrivileges(
+		hToken,
+		FALSE,
+		&tp,
+		sizeof(TOKEN_PRIVILEGES),
+		(PTOKEN_PRIVILEGES)NULL,
+		(PDWORD)NULL))
+	{
+		printf("AdjustTokenPrivileges error: %u\n", GetLastError());
+		return FALSE;
+	}
+
+	if (GetLastError() == ERROR_NOT_ALL_ASSIGNED)
+
+	{
+		printf("The token does not have the specified privilege. \n");
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+
+void LogLastError()
+{
+	DWORD   dwLastError = ::GetLastError();
+	char   lpBuffer[512] = "?";
+	if (dwLastError != 0)    // Don't want to see a "operation done successfully" error ;-)
+	::FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM,                 // It´s a system error
+					NULL,                                      // No string to be formatted needed
+					dwLastError,                               // Hey Windows: Please explain this error!
+					MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),  // Do it in the standard language
+					lpBuffer,              // Put the message here
+					(sizeof(lpBuffer) / sizeof(char)) - 1,                     // Number of bytes to store the message
+					NULL);
+	std::cout << "Last error: " << lpBuffer << std::endl;
+}
+
 bool InjectDllIntoProcess(DWORD dwPid, const std::string sInjectDll)
 {
 	bool bOk(true);
@@ -11,6 +82,7 @@ bool InjectDllIntoProcess(DWORD dwPid, const std::string sInjectDll)
 	{
 		bOk = false;
 		printf("Error: the specified process couldn't be found.\n");
+		LogLastError();
 	}
 
 	if (bOk)
@@ -67,28 +139,58 @@ bool InjectDllIntoProcess(DWORD dwPid, const std::string sInjectDll)
 	return bOk;
 }
 
+struct MatchPathSeparator
+{
+	bool operator()(char ch) const
+	{
+		return ch == '\\' || ch == '/';
+	}
+};
+
+std::string basename(const std::string& pathname)
+{
+	return std::string(
+		std::find_if(pathname.rbegin(), pathname.rend(),
+		MatchPathSeparator()).base(),
+		pathname.end());
+}
+
 std::string GetProcessName(DWORD pid)
 {
-	char buffer[MAX_PATH] = "<unknown>";
-	HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid);
+	std::string sProcessName;
+	HANDLE hProcess = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, pid);
 	if (NULL != hProcess)
 	{
 		HMODULE hMod[512];
 		DWORD cbNeeded;
-
-		if (EnumProcessModules(hProcess, hMod, sizeof(hMod), &cbNeeded))
+		char buffer[MAX_PATH] = "<unknown>";
+		DWORD dwSize = sizeof(buffer) / sizeof(char);
+		if (!QueryFullProcessImageNameA(hProcess, 0, buffer, &dwSize))
 		{
-			GetModuleBaseNameA(hProcess, hMod[0], buffer, sizeof(buffer) / sizeof(char));
+			LogLastError();
+			std::wcerr << "Failed to get processname of pid: " << pid << std::endl;
+			sProcessName = "<unknown>";
 		}
+		else
+		{
+			sProcessName = basename(std::string(buffer));
+		}
+	}
+	else
+	{
+		std::cout << "Unable to open process..." << std::endl;
+		sProcessName = "<unknown>";
 	}
 	CloseHandle(hProcess);
 
-	return std::string(buffer);
+	return sProcessName;
 }
 
 void PrintProcessInfo(DWORD pid)
 {
+#ifdef LOG_DEBUG
 	printf("%s  (PID: %u)\n", GetProcessName(pid).c_str(), pid);
+#endif
 }
 
 
@@ -97,13 +199,40 @@ int _tmain(int argc, char* argv[])
 	char path[MAX_PATH];
 	GetCurrentDirectoryA(MAX_PATH, path);
 
+	HANDLE hProc = GetCurrentProcess();
+
+	HANDLE hToken;
+	if (OpenProcessToken(hProc, TOKEN_ALL_ACCESS, &hToken))
+	{
+#ifdef LOG_DEBUG
+		std::cout << "OPen process token ok" << std::endl;
+#endif
+		if (SetPrivilege(hToken, SE_DEBUG_NAME, TRUE))
+		{
+#ifdef LOG_DEBUG
+			std::cout << "Privilige adjusted to debug" << std::endl;
+#endif
+		}
+		else
+		{
+			std::cerr << "Failed to obtaing process debugging rights, injecting in system dlls could fail" << std::endl;
+		}
+	}
+	else
+	{
+		std::cerr << "Open process token F A I L E D" << std::endl;
+	}
+
+
 	std::string sInjectDll(path);
 	sInjectDll += "\\PreventLockWorkStation_x86.dll";
 
 	std::string sProcess(argv[1]);
 	std::transform(sProcess.begin(), sProcess.end(), sProcess.begin(), ::tolower);
 
+#ifdef LOG_DEBUG
 	std::cout << "Looking for process PID of process: " << sProcess << " to hook the dll: " << sInjectDll << " into..." << std::endl;
+#endif
 
 	DWORD dwPids[1024];
 	DWORD cbNeeded;
@@ -118,7 +247,7 @@ int _tmain(int argc, char* argv[])
 
 	for (DWORD i = 0; i < dwCount; ++i)
 	{
-		if (dwPids[i] != 0)
+		if (dwPids[i] != 0 && dwPids[i] > 10)
 		{
 			PrintProcessInfo(dwPids[i]);
 			std::string sProcessName(GetProcessName(dwPids[i]));
